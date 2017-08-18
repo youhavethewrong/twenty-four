291a73392238ba4803870de8581e96bc3b94f4c2
ESC <youhavethewrong@gmail.com>
2017-08-18 15:00:18 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twenty-four "0.1.0-SNAPSHOT"
+(defproject twenty-four "0.1.0"
   :description "A 24-bit ANSI color library."
   :url "https://github.com/youhavethewrong/twenty-four.git"
   :license {:name "Eclipse Public License"
